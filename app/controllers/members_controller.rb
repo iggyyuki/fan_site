47f97d55b312a21b174a862adfc6fdeecf513198
Yuki <tigers24.backfrip.yuki1117@gmail.com>
2016-12-24 16:40:23 +0000

    ファンサイト(2) - 新規データの投稿完了

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,6 +4,22 @@ class MembersController < ApplicationController
     end
     
     def show
-        @member = Member(params[:id])
+        @member = Member.find(params[:id])
     end
+    
+    def new
+        @member = Member.new
+    end
+    
+    def create
+        @member = Member.new(member_params)
+        @member.save
+        redirect_to '/members'
+    end
+    
+    private
+        def member_params
+            params.require(:member).permit(:family_name, :given_name, :image_url)
+        end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'members' => 'members#index'
+  get 'members/new' => 'members#new'
+  post 'members' => 'members#create'
   get 'members/:id' => 'members#show'
 end
